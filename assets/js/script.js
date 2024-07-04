b54f39ef30f6e4329e66846ea2eb9158a43d52f1
Kevin Doyle <doyle.kfd@gmail.com>
2024-07-04 14:27:29 +0000

    style: added iconms for footer social links

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ console.log("Script file open");
 
 // Function called when toggle menu item is clicked 
 function toggleMenu(){
+  console.log("button click captured")
     // Get menu item by element id
     const x = document.getElementById("menu");
     // check to see if the menu is already drop down
