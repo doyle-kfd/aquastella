58e3af1a7ff62e878bd720ec5dcd380a69357a08
Kevin Doyle <doyle.kfd@gmail.com>
2024-07-04 11:36:33 +0000

    feat:add js mobile meny

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+console.log("Script file open");
+
+// Function called when toggle menu item is clicked 
+function toggleMenu(){
+    // Get menu item by element id
+    const x = document.getElementById("menu");
+    // check to see if the menu is already drop down
+    if (x.style.display === "block") {
+    // Hide menu if its open
+      x.style.display = "none";
+     } else {
+    // Open menu if closed
+     x.style.display = "block";
+     }
+}
