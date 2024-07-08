2825f18917d024f8e1cd8ccbb42137dc0d820790
Kevin Doyle <doyle.kfd@gmail.com>
2024-07-08 10:06:58 +0000

    feat/style: added reservation popup modal html/css/js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ console.log("Script file open");
 
 // Function called when toggle menu item is clicked 
 function toggleMenu(){
-  console.log("button click captured")
+  console.log("menu button click captured")
     // Get menu item by element id
     const x = document.getElementById("menu");
     // check to see if the menu is already drop down
@@ -14,3 +14,26 @@ function toggleMenu(){
      x.style.display = "block";
      }
 }
+
+// Function called when reservation button is clicked
+
+function makeReservation(){
+  console.log("capturing reservation click");
+  // Get reservation item by element id
+  const reservForm = document.getElementById("reservation-form");
+  // check to see if reservation form is already open
+  if (reservForm.style.display === "block") {
+    // hide form
+    reservForm.style.display = "none";
+  } else {
+    // open if closed 
+    reservForm.style.display = "block";
+  }
+  }
+
+  // close reservation form
+  const reservForm = document.getElementById("reservation-form");
+  function closeReservation() {
+    console.log("captured close reserv form");
+    reservForm.style.display = "none";
+  }
