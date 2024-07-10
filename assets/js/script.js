27657fd011683cc7fe6cfdc0058142d9d8a6fce8
Kevin Doyle <doyle.kfd@gmail.com>
2024-07-10 15:59:50 +0000

    code: find table - get values - numguests, sitting, date

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,82 @@ function toggleMenu(){
      }
 }
 
+
+
+
+  // close reservation form if close button pressed
+  const reservForm = document.getElementById("reservation-form");
+  function closeReservation() {
+    console.log("captured close reserv form");
+    reservForm.style.display = "none";
+  }
+
+/**
+ * Get form field Values
+ */
+
+let fnamInput = document.getElementById('fname'); // first name from form
+let lnameInput = document.getElementById('lname'); // last name from form
+let emailInput = document.getElementById('email'); // Email Address from form
+let teleInput = document.getElementById('telephone'); // last name
+let findTable = document.getElementById('ftable'); // find table button - used by listner
+
+
+
+/**
+ * Initialse values for reservation checks.
+ */
+let guestNumbers = 0;
+let sitting = "";
+let date = "";
+let firstname = "";
+let lastname = "";
+let email = "";
+let phone = "";
+let confirmationNumber = 0;
+const reservations = [];
+const max_sitting_Seats = 16;
+
+
+
+
+
+/**
+ *  create reservation object for storing values
+ */ 
+// createreservationObj();
+
+/**
+ *  Create Listner for "find a table button click"
+ *
+ *  Step 1:
+ *        it takes, number of guests, sitting and date and checks to see if there are seats available.
+ *        If there are seats it goes to step 2
+ * 
+ *  Step 2:
+ *        produce form part 2 and get firstname, lastname, email and telephone number
+ *        write reservation to array and give customer reservation number
+ */
+findTable.addEventListener("click", () => {
+  console.log("picked up find table button click");
+  openReservation(); // Open Form Tab 1
+
+  // Get values from form tab1
+  let numGuests = document.getElementById('guestNum').value; // number of guests from form
+  let dateInput = document.getElementById('ddate').value;// reservation date from form
+  let timeInput = document.getElementById('dtime').value; // reservation time from form
+
+  // Check to see if a reservation exists
+  if (reservations.length === 0){
+    console.log("numguests contains " + numGuests);
+    console.log("Date of reservation " + dateInput);
+    console.log("Sitting " + timeInput);
+  }
+
+
+});
+
+
 /**
  *    Function opens form tab 1 when "reservation" button is clicked.
  *    It displays tab 1 first. guestnum, sitting, date
@@ -31,98 +107,9 @@ function openReservation(){
   reservForm.style.display = "block"; // Display the reservation form
   tab1.style.display = "block"; // Display the stage 1 form
   tab2.style.display = "none"; // Hide the stage 2 form until completing reservation
-  
-
-
-
-  /* ////////////////////////// Look to delete before finishing project /////////////// */
-  /* // Get reservation item by element id
-  const reservForm = document.getElementById("reservation-form");
-  // check to see if reservation form is already open
-  if (reservForm.style.display === "block") {
-    // hide form
-    reservForm.style.display = "none";
-  } else {
-    // open if closed 
-    reservForm.style.display = "block";
-    // create variable to store reservation tabs
-    const resTab = 0;
-    console.log("defining resTab");
-    // run function to display tab 1
-    showresTab(resTab);
-    function showresTab(n) {
-      console.log("trying to display tab");
-      // create variable to store tab contents
-      const x = document.getElementsByClassName("res-tab");
-      // Display tab 1
-      x[n].style.display = "block";
-      // hide tab 2 while content is being filled in and reservation checked
-      x[n+1].style.display = "none";
-    }
-  } */
+   
   };
 
-
-  // close reservation form if close button pressed
-  const reservForm = document.getElementById("reservation-form");
-  function closeReservation() {
-    console.log("captured close reserv form");
-    reservForm.style.display = "none";
-  }
-
-/**
- * Get form field Values
- */
-let numGuests = document.getElementById('guestNum'); // number of guests from form
-let dateInput = document.getElementById('ddate');// reservation date from form
-let timeInput = document.getElementById('dtime'); // reservation time from form
-let fnamInput = document.getElementById('fname'); // first name from form
-let lnameInput = document.getElementById('lname'); // last name from form
-let emailInput = document.getElementById('email'); // Email Address from form
-let teleInput = document.getElementById('telephone'); // last name
-let findTable = document.getElementById('ftable'); // find table button - used by listner
-
-
-
-
-
-
-/**
- *  Create Listner for "find a table button click"
- *
- *  Step 1:
- *        it takes, number of guests, sitting and date and checks to see if there are seats available.
- *        If there are seats it goes to step 2
- * 
- *  Step 2:
- *        produce form part 2 and get firstname, lastname, email and telephone number
- *        write reservation to array and give customer reservation number
- */
-findTable.addEventListener("click", () => {
-   console.log("picked up find table button click");
-   // create reservation object for storing values
-   createreservationObj();
-
-  // Check to see if the date is available, then if there are seats available at that sitting
-  // Check if there are any bookings yet
-  if (reservations.length === 0) {
-    console.log("no records in the data")
-    // Go to second form page and get name, email and telephone - then write the reservation to the array
-    completeReservation();
-
-  };
-
-
-  
-  // Convert date from yyyy-mm-dd to dd-mm-yy - will be needed to display in dashboard
-  convertDate();
-
-
-
- 
-
-
-});
 
 
 /**
@@ -158,34 +145,24 @@ function convertDate () {
   console.log(newDate);
 }
 
-/**
- * Initialse values for reservation checks.
- */
-let guestNumbers = 0;
-let sitting = "";
-let firstname = "";
-let lastname = "";
-let email = "";
-let phone = "";
-let confirmationNumber = 0;
-const reservations = [];
-const max_sitting_Seats = 16;
+
 
 /**
  * Function to create new reservation object
- */
+
 function createreservationObj() {
   const newReservation = {
-    guestNumbers: guestNumbers,
-    sitting: sitting,
-    firstName: firstname,
-    lastName: lastname,
-    email: email,
-    phone: phone,
+    guestNumbers: numGuests.value,
+    date: dateInput.value,
+    sitting: timeInput.value,
+    firstName: fnamInput.value,
+    lastName: lnameInput.value,
+    email: emailInput.value,
+    phone: teleInput.value,
     confirmationNumber: generateConfirmationNumber() //generate reservation number function
   };
 }
-
+ */
 
 /**
  *  Function to create reservation confirmation number
@@ -193,3 +170,19 @@ function createreservationObj() {
 function generateConfirmationNumber(){
   return reservations.length + 1;
 }
+
+
+
+
+
+
+//////////////////////////////   Use as needed ////////////////////////////
+/*
+    console.log("guestNumbers");
+    console.log("sitting");
+    console.log("firstname");
+    console.log("lastname");
+    console.log("email");
+    console.log("phone");
+    console.log("confirmationNumber");
+*/
