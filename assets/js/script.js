0f39aea042ebc7d1d8ebcd139258235198a1089d
Kevin Doyle <doyle.kfd@gmail.com>
2024-07-13 12:51:58 +0000

    style:reworded the text in the weekly reservation counters

--- assets/js/script.js
+++ assets/js/script.js
@@ -454,12 +454,12 @@ function updateStats(reservations) {
   });
 
   // Update the counter text inner with the incremented counter values
-  document.getElementById('totalReservationsToday').innerText = `Total Reservations Today: ${totalToday}`;
+  document.getElementById('totalReservationsToday').innerText = `Total Today: ${totalToday}`;
   document.getElementById('firstSittingToday').innerText = `First Sitting: ${firstSittingToday}`;
   document.getElementById('secondSittingToday').innerText = `Second Sitting: ${secondSittingToday}`;
-  document.getElementById('totalReservationsNext7Days').innerText = `Total Reservations Next 7 Days: ${totalNext7Days}`;
-  document.getElementById('firstSittingNext7Days').innerText = `First Sitting Next 7 Days: ${firstSittingNext7Days}`;
-  document.getElementById('secondSittingNext7Days').innerText = `Second Sitting Next 7 Days: ${secondSittingNext7Days}`;
+  document.getElementById('totalReservationsNext7Days').innerText = `Total Next 7 Days: ${totalNext7Days}`;
+  document.getElementById('firstSittingNext7Days').innerText = `First Sitting: ${firstSittingNext7Days}`;
+  document.getElementById('secondSittingNext7Days').innerText = `Second Sitting: ${secondSittingNext7Days}`;
   
   
   drawDailyChart(totalToday, firstSittingToday, secondSittingToday);
