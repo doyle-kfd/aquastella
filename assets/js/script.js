0824d14b923dc06e274d8c24dc73d6b6151b1642
Kevin Doyle <doyle.kfd@gmail.com>
2024-07-13 05:49:36 +0000

    feat/code: added html to admin page to display counters and added JS to calculate counters for today and the next week

--- assets/js/script.js
+++ assets/js/script.js
@@ -351,8 +351,6 @@ function updateAdminPage() {
 }
 
 
-
-
 // Add event listner to check when DOM is fully loaded.
 document.addEventListener('DOMContentLoaded', function() {
   updateReservationsTable();
@@ -380,20 +378,83 @@ function updateReservationsTable() {
   tableBody.innerHTML = '';
 
   // Populate the table with reservations
-  // For every reservation in the array
   reservations.forEach(reservation => {
-      const row = document.createElement('tr'); // create a new table row element
+      const row = document.createElement('tr');
 
-      // for each property key - eg date, sitting guests
-      for (const key in reservation) { 
-          const cell = document.createElement('td'); // create a new table cell element
-          cell.innerText = reservation[key]; // set the value in the table cell to the reservation key value eg. date 2024-07-12
-          row.appendChild(cell); // append the cell to the current table row
+      for (const key in reservation) {
+          const cell = document.createElement('td');
+          cell.innerText = reservation[key];
+          row.appendChild(cell);
       }
 
-      tableBody.appendChild(row); // append the row to the table in admin page
+      tableBody.appendChild(row);
   });
+
+  // Update the stats
+  updateStats(reservations);
 }
+
+// Update the reservation stats for displaying
+function updateStats(reservations) {
+  // Get todays date and as an ISOString and split it on the T to give yyyy-mm-dd
+  const today = new Date().toISOString().split('T')[0];
+
+  // Initialise the variable next7Days 
+  const next7Days = new Date();
+  
+  // set the value of next7Days to todays date + 7
+  next7Days.setDate(next7Days.getDate() + 7);
+
+  // Set the variavle next7DaysISOString to newt7Days and split on the T to give yyyy-mm-dd
+  const next7DaysISOString = next7Days.toISOString().split('T')[0];
+
+  // Initialise the values for the stat counters
+  let totalToday = 0;
+  let firstSittingToday = 0;
+  let secondSittingToday = 0;
+  let totalNext7Days = 0;
+  let firstSittingNext7Days = 0;
+  let secondSittingNext7Days = 0;
+
+  // Create loop checks on array date to see if there are reservations
+  // The calculations will be used for output to counters
+  // For each reservation starting with [0]
+  reservations.forEach(reservation => {
+      
+    // Check and see if the date is today
+      if (reservation.date === today) {
+          totalToday++; // If it is, increment todays date counter by 1
+          // Check to see of there are reservations for first sitting
+          if (reservation.sitting === 'First - 17:00') {
+              firstSittingToday++; // If there are, then increment first sitting by 1
+          } else if (reservation.sitting === 'Second - 20:30') {  // Check to see if the resevation is for second sitting
+              secondSittingToday++; // increment counter by 1
+          }
+      }
+
+      // Calculate counters for next 7 days
+      // Check to see if the reservation date is greater than today and its less than the calculated ISO string
+      if (reservation.date > today && reservation.date <= next7DaysISOString) {
+          totalNext7Days++; // If it is then increment the counter for the total next 7 days.
+          // Check to see if the reservation is for the first sitting
+          if (reservation.sitting === 'First - 17:00') {
+              firstSittingNext7Days++; // If it is, then increment
+          } else if (reservation.sitting === 'Second - 20:30') { // check to see if the reservation is for the second sitting
+              secondSittingNext7Days++; // Increment the counter by 1
+          }
+      }
+  });
+
+  // Update the counter text inner with the incremented counter values
+  document.getElementById('totalReservationsToday').innerText = `Total Reservations Today: ${totalToday}`;
+  document.getElementById('firstSittingToday').innerText = `First Sitting Today: ${firstSittingToday}`;
+  document.getElementById('secondSittingToday').innerText = `Second Sitting Today: ${secondSittingToday}`;
+  document.getElementById('totalReservationsNext7Days').innerText = `Total Reservations Next 7 Days: ${totalNext7Days}`;
+  document.getElementById('firstSittingNext7Days').innerText = `First Sitting Next 7 Days: ${firstSittingNext7Days}`;
+  document.getElementById('secondSittingNext7Days').innerText = `Second Sitting Next 7 Days: ${secondSittingNext7Days}`;
+}
+
+
 
 
 
