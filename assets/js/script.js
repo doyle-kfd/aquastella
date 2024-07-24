fb08edf6d2910d7a6e0065d19a8864fd012289ad
Kevin Doyle <doyle.kfd@gmail.com>
2024-07-24 06:51:17 +0000

    fix: added missing operator in reservation array creation on first visit

--- assets/js/script.js
+++ assets/js/script.js
@@ -259,7 +259,7 @@ document.addEventListener("DOMContentLoaded", function () {                 // C
             */
 
             // refactored formatDate function assignment
-            const formatDate (date) => {
+            const formatDate = (date) => {
                 const day = String(date.getDate()).padStart(2, '0');
                 const month = String(date.getMonth() + 1).padStart(2, '0');                 // January is 0!
                 const year = date.getFullYear();
